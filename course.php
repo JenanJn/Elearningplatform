4bfe3e4e70b2c71094ed9b169bc4cdcc6c078dd8
JenanJn <jenanjn98@gmail.com>
2024-07-14 13:02:38 +0530

    course.php

---
+++ course.php
@@ -0,0 +1,41 @@
+<?php
+class Course {
+    private $conn;
+    private $table_name = "courses";
+
+    public $id;
+    public $title;
+    public $description;
+    public $created_at;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function read() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function create() {
+        $query = "INSERT INTO " . $this->table_name . " SET title=:title, description=:description";
+        $stmt = $this->conn->prepare($query);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":description", $this->description);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // Other CRUD operations
+}
+?>
